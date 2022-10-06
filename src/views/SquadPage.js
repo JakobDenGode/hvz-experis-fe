09e9c1d2019003e0873d93293369c019257a409c
Carl Markus <55441180+CarlMarkus@users.noreply.github.com>
2022-10-06 11:47:34 +0200

    started on squad api

--- src/views/SquadPage.js
+++ src/views/SquadPage.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SquadList from "../components/squad-list/SquadList";
 import Heading from "../common/Heading";
 
+const apiUrl = "https://hvz-api-noroff.herokuapp.com/game";
+
+//const [squad, setSquad] = useState([]);
+
 function SquadPage() {
+  useEffect(() => {
+    console.log("squad");
+    const findSquads = async () => {
+      try {
+        const response = await fetch(`${apiUrl}`);
+
+        if (!response.ok) throw new Error("Could not complete request");
+        const data = await response.json();
+        console.log(data);
+        return [null, data];
+      } catch (error) {
+        return [error.message, []];
+      }
+    };
+
+    findSquads();
+  }, []);
+
   const squads = [
     {
       name: "Squad1",
