9814ad522dd3af2aeb5961cecfc9c6401c1ae2a9
Carl Markus <55441180+CarlMarkus@users.noreply.github.com>
2022-10-07 11:13:06 +0200

    example code to display map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Map() {
-  return <p>test</p>;
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
 export default Map;
