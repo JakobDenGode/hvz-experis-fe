1428728eb596fc8fe1bd05a44f1dac2ec853f3d5
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-10-07 10:56:58 +0200

    test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithRedirectCallback = ({ children, ...props }) => {
   const navigate = useNavigate();
+
   const onRedirectCallback = (appState) => {
     navigate((appState && appState.returnTo) || window.location.pathname);
   };
