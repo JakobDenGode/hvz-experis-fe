32cecc77ecb78468325bccffb33705b1810c68ea
Carl Markus <55441180+CarlMarkus@users.noreply.github.com>
2022-10-07 11:39:54 +0200

    Started styling the map , basic  move functionality

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -3,7 +3,12 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      height={180}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
