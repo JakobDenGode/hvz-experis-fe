feabf8d1809ef8c6d3d27f1f35777fee0ff5e58b
Carl Markus <55441180+CarlMarkus@users.noreply.github.com>
2022-10-13 18:10:15 +0200

    get all missions

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -11,16 +11,20 @@ import {
 } from "react-leaflet";
 import { createHeaders } from "../admin/CreateHeaders";
 import { useAuth0 } from "@auth0/auth0-react";
+import { divIcon } from "leaflet";
 
 function Map() {
   const { getAccessTokenSilently } = useAuth0();
   const gameId = useParams();
   const [gameData, setGame] = useState([]);
-  const [cords, setCords] = useState([0, 0]);
+  const [cords, setCords] = useState([]);
+  const [missionCords, setMissionCords] = useState([]);
   const [getRectangle, setRectangle] = useState([
     [0, 0],
     [0, 0],
   ]);
+
+  const fillRedOptions = { fillColor: "red" };
 
   useEffect(() => {
     const findGames = async () => {
@@ -49,8 +53,70 @@ function Map() {
     findGames();
   }, []);
 
-  console.log("test rectangle:");
-  console.log(getRectangle);
+  //Get missions
+  useEffect(() => {
+    const findGames = async () => {
+      const accessToken = await getAccessTokenSilently();
+      try {
+        const response = await fetch(
+          `https://hvz-api-noroff.herokuapp.com/game/1/mission/1`,
+          { headers: createHeaders(accessToken) }
+        );
+        //if (!response.ok) throw new Error("Could not complete request");
+        console.log(response);
+        const data = await response.json();
+
+        console.log("-------");
+        console.log(data);
+        console.log("here");
+        data.map((item) =>
+          console.log(item.missionName, item.missionLat, item.missionLng)
+        );
+
+        setMissionCords(data);
+        console.log(missionCords);
+
+        //get coordinates to draw rectangle
+        return [null, data];
+      } catch (error) {
+        return [error.message, []];
+      }
+    };
+    findGames();
+  }, []);
+
+  console.log("++++");
+  console.log(missionCords);
+
+  console.log("------");
+  missionCords.map((item) => console.log(item.missionLat, item.missionLng));
+
+  /*
+  cords.map((item) => console.log(item.missionLat, item.missionLng));
+
+  console.log(testyBoi);
+  */
+
+  /*
+  return missionCords.map(item => {
+    <Marker position={item.missionLat, item.missionLng} />
+    
+})
+}
+
+*/
+
+  //Function to get all mission markers coordinaters
+  function MultipleMarkers() {
+    const map = useMapEvent({
+      click() {
+        map.locate();
+      },
+    });
+    return missionCords.map((item) => {
+      return <Marker position={[item.missionLat, item.missionLng]} />;
+    });
+  }
 
   return (
     <>
@@ -65,9 +131,10 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={cords}></Marker>
+
+        <MultipleMarkers />
         {/* test marker/ for missions */}
-        <Marker position={[59.92295744566574, 10.739275233893595]} />
+        <Marker position={[59.931145, 10.79683]} />
         {/*Rectangle*/}
         <Rectangle
           bounds={getRectangle}
