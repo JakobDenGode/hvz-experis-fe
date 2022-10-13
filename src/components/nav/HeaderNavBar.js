8c9d83de64a8496f25dc9d12f6b1c0aad8a56080
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-10-13 15:40:53 +0200

    added top nav bar

--- src/components/nav/HeaderNavBar.js
+++ src/components/nav/HeaderNavBar.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+import { Button, Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { usePlayer } from "../../context/PlayerContext";
 
 function HeaderNavBar({ title }) {
+  const navigate = useNavigate();
+  const { player, setPlayer } = usePlayer();
+  console.log(player);
+
+  function goToLanding() {
+    navigate("/");
+  }
+
   return (
     <Nav className="mx-auto w-100 bg-light justify-content-around">
-      <div>Exit</div>
-      <div>{title}</div>
-      <div>Bite Code</div>
+      <div onClick={goToLanding}>Exit</div>
+      <h2>{title}</h2>
+      {player && (
+        <Button
+          className={`${
+            player.type === "zombie" ? "btn-secondary" : "btn-primary"
+          }`}
+        >
+          Bite Code
+        </Button>
+      )}
     </Nav>
   );
 }
