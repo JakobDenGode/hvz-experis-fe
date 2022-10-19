5dfbad61ee22c997728ed45d5878141d84b4fcd9
Carl Markus <55441180+CarlMarkus@users.noreply.github.com>
2022-10-19 09:35:08 +0200

    fixed admin restriction

--- src/views/EditPage.js
+++ src/views/EditPage.js
@@ -7,13 +7,12 @@ export default withAuthenticationRequired(AdminPage, {
 });
 
 function AdminPage() {
-  /*
   const { user } = useAuth0();
   console.log(user);
   if (user && user["http://demozero.net/roles"].length === 0) {
     return <div>Permission denied</div>;
   }
-  */
+
   return (
     <div>
       <EditList />
